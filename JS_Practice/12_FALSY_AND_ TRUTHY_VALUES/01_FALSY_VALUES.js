a59c3ea3417fead5002868c7abca76cea204488e
Ayush Choubey <ayushgtg@outlook.com>
2025-06-16 03:36:51 +0530

    learn FALSY and TRUTH Values

---
+++ JS_Practice/12_FALSY_AND_ TRUTHY_VALUES/01_FALSY_VALUES.js
@@ -0,0 +1,18 @@
+/* 
+
+FALSY VALUES :- values which act false in conditional statement
+
+                types of falsy values are:-
+                                        1)numbers: 0,-0
+                                        2)empty string: ""
+                                        3)NULL value: null
+                                        4)UNDEFINED VALUE : undefined
+                                        5)Not a Number : NaN 
+*/
+
+if(!0)console.log(" 0 and -0 act as falsy value ")
+if(!"")console.log(" EMPTY string  is a falsy value ")
+if(!null)console.log(" NULL is a falsy value ")
+if(!NaN)console.log(" NaN is a falsy value ") 
+
+
---
+++ JS_Practice/12_FALSY_AND_ TRUTHY_VALUES/02_TRUE_VALUES.js
@@ -0,0 +1,15 @@
+/* 
+
+TRUE VALUES :- values which act as TRUE in conditional statement
+
+                types of falsy values are:-
+                                        1)numbers: except (0,-0)
+                                        2)non-empty string: ("  ") or (" _dfjdf ") or "0"
+                                        3)empty Objects ,Arrays : {} or []
+                                        4)Functions :
+*/
+
+if (-464) console.log(" negative value is truthy");
+if ([]) console.log(" empty array is truthy");
+if ({}) console.log(" empty object is truthy");
+if ("0") console.log(" string with 0 is truthy");
